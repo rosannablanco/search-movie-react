b7fa3e781efde6efa1de20c96057257345a0cd1a
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-06-04 12:10:22 +0200

    Update docs

---
+++ docs/precache-manifest.aadfa55779a2891e1c102a03ccb0e572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2dc2e7fce6a643e9303e9b44e48349",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9827c42acf226d66a07",
+    "url": "./static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "8ef7369f82a23c8577c2",
+    "url": "./static/css/main.48f1c2db.chunk.css"
+  },
+  {
+    "revision": "b9827c42acf226d66a07",
+    "url": "./static/js/2.bc8a5fc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.bc8a5fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef7369f82a23c8577c2",
+    "url": "./static/js/main.abdb6617.chunk.js"
+  },
+  {
+    "revision": "819b344c798b1b8dba09",
+    "url": "./static/js/runtime-main.ab8bd17a.js"
+  }
+]);
