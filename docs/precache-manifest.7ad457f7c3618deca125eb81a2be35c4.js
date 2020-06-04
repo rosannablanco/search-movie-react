9058a8c453846efdb4dd07f6f0f641eaf614db59
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-06-04 12:50:13 +0200

    new path homepage package.json

---
+++ docs/precache-manifest.7ad457f7c3618deca125eb81a2be35c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2e4c3b641cbf514a5b8b39509e21a0",
+    "url": "/rosannablanco/search-movie-react/index.html"
+  },
+  {
+    "revision": "b9827c42acf226d66a07",
+    "url": "/rosannablanco/search-movie-react/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "8b535faa19d62e77d409",
+    "url": "/rosannablanco/search-movie-react/static/css/main.48f1c2db.chunk.css"
+  },
+  {
+    "revision": "b9827c42acf226d66a07",
+    "url": "/rosannablanco/search-movie-react/static/js/2.bc8a5fc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rosannablanco/search-movie-react/static/js/2.bc8a5fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b535faa19d62e77d409",
+    "url": "/rosannablanco/search-movie-react/static/js/main.bd6ab8b8.chunk.js"
+  },
+  {
+    "revision": "35cc9e7574311a671ffa",
+    "url": "/rosannablanco/search-movie-react/static/js/runtime-main.1e611eb3.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a30c74d8b27cb88d74477d2b3a0be3fc.js"
+  "/rosannablanco/search-movie-react/precache-manifest.7ad457f7c3618deca125eb81a2be35c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rosannablanco/search-movie-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
